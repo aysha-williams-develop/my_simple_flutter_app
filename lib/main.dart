375ceaf3796c4bdb90c78e2d44e1a75434775af3
Aysha Williams <awilliams@mavenlink.com>
2019-08-02 17:24:19 -0600

    Add theme to app
    - Added the Scaffold widget, a Material Component,
    so that I can take advantage of the Material Theme system
    (only Material Components, will use the Material Theme, the
    Text Component is not a Material Component, and thus, by itself,
    it will not use a theme, it must be placed insdie fo the Material
    Component).

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      home: new Text('Hello World'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Sample Code'),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Container(height: 50.0,),
+        ),
+      ),
     );
   }
 }
