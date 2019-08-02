4f083cf14039452b8c1601a88017a9aa9ad62037
Aysha Williams <awilliams@mavenlink.com>
2019-08-02 17:58:41 -0600

    Update app theme - Updated the app theme using ThemeData - The theme is darker now

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      theme: ThemeData(
+          brightness: Brightness.dark,
+          primaryColor: Colors.lightBlue[800],
+          accentColor: Colors.cyan[600]
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: Text('My Simple Flutter App'),
